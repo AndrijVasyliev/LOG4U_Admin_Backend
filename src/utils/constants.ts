e685791f3889dec0a20c86f01739cb4efdb963e6
andrii.vasyliev <aa5856bk@gmail.com>
2023-07-25 23:54:10 +0300

    feat(LOG-13): set owner pagination to 50 per page

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ export const HEALTH_MEMORY_RSS_LIMIT = 150 * 1024 * 1024;
 export const MONGO_UNIQUE_INDEX_CONFLICT = 11000;
 
 export const OWNER_DEFAULT_LIMIT = 0;
-export const OWNER_DEFAULT_OFFSET = 25;
+export const OWNER_DEFAULT_OFFSET = 50;
 
 export const LangPriorities = ['EN', 'UA', 'ES', 'RU'] as const;
