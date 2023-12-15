ecf2d81897fe2a0d892580dcedc9d84fff1dd7d0
Kozak_Mamaj <aa5856bk@gmail.com>
2023-12-15 23:51:14 +0200

    feat(LOG): add response logger

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,7 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Request, Response, Send } from 'express';
 
 import { LoggerService } from './logger.service';
+// ToDo Refactor response body logger
+const resDotSendInterceptor = (res: Response, send: Send) => {
+  const sendFunc = (content: any) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    res.contentBody = content;
+    res.send = send;
+    res.send(content);
+  };
+  return sendFunc;
+};
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -12,12 +23,20 @@ export class LoggerMiddleware implements NestMiddleware {
     log.http(
       `${req.method}: ${req.hostname}(${req.ip})${req.originalUrl} -> ${
         req.socket.localAddress
-      }:${req.socket.localPort}, ${JSON.stringify(req.headers)}`,
+      }:${req.socket.localPort}, ${JSON.stringify(
+        req.headers,
+      )}, ${JSON.stringify(req.body)}`,
     );
-    next();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    res.send = resDotSendInterceptor(res, res.send);
     res.on('finish', () => {
       const rt = res.get('X-Response-Time');
       log.http(`${rt} Response code: ${res.statusCode}`);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      log.debug(`Response body: ${JSON.stringify(res.contentBody)}`);
     });
+    next();
   }
 }
