7ddc7d4e035ed5d7a45b899cf5e786b5374ef149
Kozak_Mamaj <aa5856bk@gmail.com>
2023-12-18 23:54:38 +0200

    feat(LOG): change logger

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -4,14 +4,13 @@ import { Request, Response, Send } from 'express';
 import { LoggerService } from './logger.service';
 // ToDo Refactor response body logger
 const resDotSendInterceptor = (res: Response, send: Send) => {
-  const sendFunc = (content: any) => {
+  return (content: any) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     res.contentBody = content;
     res.send = send;
     res.send(content);
   };
-  return sendFunc;
 };
 
 @Injectable()
@@ -21,21 +20,28 @@ export class LoggerMiddleware implements NestMiddleware {
   public use(req: Request, res: Response, next: () => void) {
     const log = this.logger;
     log.http(
-      `${req.method}: ${req.hostname}(${req.ip})${req.originalUrl} -> ${
-        req.socket.localAddress
-      }:${req.socket.localPort}, ${JSON.stringify(
-        req.headers,
-      )}, ${JSON.stringify(req.body)}`,
+      `${req.method}: ${req.hostname}(${req.ip})${req.originalUrl} -> ${req.socket.localAddress}:${req.socket.localPort}`,
     );
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     res.send = resDotSendInterceptor(res, res.send);
     res.on('finish', () => {
       const rt = res.get('X-Response-Time');
-      log.http(`${rt} Response code: ${res.statusCode}`);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      log.debug(`Response body: ${JSON.stringify(res.contentBody)}`);
+      const login = req.get('X-User-Login');
+      const deviceId = req.get('X-Device-Id');
+      log.http(
+        `${rt} ${req.method}:${req.originalUrl}${
+          login ? ', User: ' + login : ''
+        }${deviceId ? ', Device: ' + deviceId : ''}, Response code: ${
+          res.statusCode
+        }${
+          res.statusCode < 200 || res.statusCode >= 300
+            ? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              'Response body: ' + JSON.stringify(res.contentBody)
+            : ''
+        }`,
+      );
     });
     next();
   }
