3bd66f9c0a460e3f6f96638726cbaa1fcbf5585a
Kozak_Mamaj <aa5856bk@gmail.com>
2024-01-16 19:58:17 +0200

    feat(LOG): app sw start

---
+++ static/register_sw.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",(()=>{"serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js").then((()=>{console.log("Service worker registered!")})).catch((e=>{console.warn("Error registering service worker:"),console.warn(e)}))}));
