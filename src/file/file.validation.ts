86606b7ddf3a5d05565cc2d52e67b458357b581c
Kozak_Mamaj <aa5856bk@gmail.com>
2024-04-11 10:52:03 +0300

    feat(LOG): fixes for file

--- src/file/file.validation.ts
+++ src/file/file.validation.ts
@@ -1,20 +1,30 @@
 import * as Joi from 'joi';
 import { MongoObjectIdValidation } from '../utils/idValidate.pipe';
-import { FILE_OF_TYPES } from '../utils/constants';
+import {
+  FILE_OF_TYPES,
+  MAX_FILE_COMMENT_LENGTH,
+  MIN_FILE_COMMENT_LENGTH,
+} from '../utils/constants';
 
 export const CreateFileValidation = Joi.object({
   linkedTo: MongoObjectIdValidation.required(),
   fileOf: Joi.string()
     .valid(...FILE_OF_TYPES)
     .required(),
-  comment: Joi.string().optional(),
+  comment: Joi.string()
+    .min(MIN_FILE_COMMENT_LENGTH)
+    .max(MAX_FILE_COMMENT_LENGTH)
+    .optional(),
 });
 
 export const FileQueryParamsSchema = Joi.object({
   offset: Joi.number().integer().min(0).optional(),
   limit: Joi.number().integer().min(1).optional(),
   filename: Joi.string().optional(),
-  comment: Joi.string().optional(),
+  comment: Joi.string()
+    .min(MIN_FILE_COMMENT_LENGTH)
+    .max(MAX_FILE_COMMENT_LENGTH)
+    .optional(),
 })
   .keys({
     orderby: Joi.string().valid('filename', 'createdAt'),
